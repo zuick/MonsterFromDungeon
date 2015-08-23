fe3daecd3dd7cea984b4a5f61b02fd3ad8fa2a01
Ruslan Khakimov <izuick@gmail.com>
2015-08-23 15:58:52 +0500

    Ignore BodyPart Layer collision

---
+++ Assets/Scripts/Person/PersonBody.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonBody : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreLayerCollision (8, 8, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
