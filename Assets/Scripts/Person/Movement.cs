90c1ed5e9dd705302cd2636faed58d9366376770
Тимур Насыров <Timmy@MacBook-Pro-Timur.local>
2015-08-25 04:21:58 +0500

    added lukes

--- Assets/Scripts/Person/Movement.cs
+++ Assets/Scripts/Person/Movement.cs
@@ -11,6 +11,22 @@ public class Movement : MonoBehaviour {
 	void Start () {
 		climbing = GetComponent<Climbing> ();
 		rigidbody2D = GetComponent<Rigidbody2D> ();
+		if (gameObject.tag == "Player") {
+			PreventCollisionsWithLukes();
+		}
+	}
+
+	void PreventCollisionsWithLukes()
+	{
+		GameObject[] lukes = GameObject.FindGameObjectsWithTag("Luke");
+		foreach (var luke in lukes) {
+			foreach(var collider in gameObject.GetComponents<Collider2D>()) {
+				Physics2D.IgnoreCollision(collider, luke.GetComponent<Collider2D>(), true);
+			}
+			foreach(var childCollider in gameObject.GetComponentsInChildren<Collider2D>()) {
+				Physics2D.IgnoreCollision(childCollider, luke.GetComponent<Collider2D>(), true);
+			}
+		}
 	}
 	
 	// Update is called once per frame
