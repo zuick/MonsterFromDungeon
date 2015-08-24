cf2e412c4ab55f2833d11ec60eb50f1aec9bf4ca
Ruslan Khakimov <izuick@gmail.com>
2015-08-25 02:28:11 +0500

    Grab check for Citizen

---
+++ Assets/Scripts/Person/Grabing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool grab = Input.GetButtonDown("Fire1");
+		
+		if (grab) {
+			Collider2D[] objects = Physics2D.OverlapCircleAll( transform.position, 0.1f );
+			foreach( Collider2D col in objects ){
+				if( col.transform.CompareTag("Citizen") )
+					Debug.Log ("GRAB");
+			}
+		}
+	}
+}
